c85793a1026124eed7c32d5bff049f7282b80e5d
Zachary Carpenter <zaca1902@colorado.edu>
2025-04-22 10:19:09 -0600

    PCA and CART stuff

---
+++ CARTMod4Flow.R
@@ -0,0 +1,13 @@
+CARTMod4Flow <- function(flowdata_gage, sst_pcs, gagename)
+{
+  cart <- rpart(flowdata_gage ~., data = sst_pcs, method = "anova")
+  
+  cartplot <- rpart.plot(cart, type = 3, extra = 101, under = TRUE, 
+                         tweak = 1.2, 
+                         main = paste("CART on Annual Spring Streamflow for ", 
+                                      gagename, 
+                                      "\nUsing winter SST PCs (4) as Covariates"))
+  return(list(
+    mod = cart,
+    plot = cartplot))
+}
