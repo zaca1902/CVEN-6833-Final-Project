d8c9b7d7a9c017096819b3341622badb559a5647
Zachary Carpenter <zaca1902@colorado.edu>
2025-04-22 10:49:02 -0600

    CART fit and plot

--- CARTMod4Flow.R
+++ CARTMod4Flow.R
@@ -2,11 +2,13 @@ CARTMod4Flow <- function(flowdata_gage, sst_pcs, gagename)
 {
   cart <- rpart(flowdata_gage ~., data = sst_pcs, method = "anova")
   
-  cartplot <- rpart.plot(cart, type = 3, extra = 101, under = TRUE, 
+  rpart.plot(cart, type = 3, extra = 101, under = TRUE, 
                          tweak = 1.2, 
                          main = paste("CART on Annual Spring Streamflow for", 
                                       gagename, 
                                       "\nUsing Winter SST PCs (4) as Covariates"))
+  cartplot <- recordPlot()
+  
   return(list(
     mod = cart,
     plot = cartplot))
