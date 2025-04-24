1d891f322fb617d2c40bcd9c9ff81c841e94181d
Zachary Carpenter <zaca1902@colorado.edu>
2025-04-24 10:29:21 -0600

    revisions to prediction and evaluation code

--- modeval.R
+++ modeval.R
@@ -12,13 +12,13 @@ for (i in 1:length(actvals)){
 # Boxplot the Correlation and RMSE
 corrplot <- ggplot()+
   geom_boxplot(mapping = aes(x = "Correlation", corrskill))+
-  labs(title = "Corr. - CART", x = "", y = "")+
+  labs(title = "Corr.", x = "", y = "")+
   theme(plot.title = element_text(size = 13, face = "bold", hjust = 0.5))+
   coord_cartesian(ylim = c(-0.5,2))
 
 rmseplot <- ggplot()+
   geom_boxplot(mapping = aes(x = "RMSE", rmseskill))+
-  labs(title = "RMSE - RF", x = "", y = "")+
+  labs(title = "RMSE", x = "", y = "")+
   theme(plot.title = element_text(size = 13, face = "bold", hjust = 0.5))+
   coord_cartesian(ylim = c(0,5000))
 
