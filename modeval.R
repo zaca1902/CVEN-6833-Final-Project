a8f7ba356280b99a29ff9b0218a233693aae4073
Zachary Carpenter <zaca1902@colorado.edu>
2025-04-29 11:12:44 -0600

    major revisions!!!

--- modeval.R
+++ modeval.R
@@ -1,24 +1,28 @@
 modeval <- function(actvals, predvals){
 # Calculate Correlation and RMSE
-corrskill = 1:length(actvals)
-rmseskill = 1:length(actvals )
+n <- length(actvals)
+corrskill <- numeric(n)
+rmseskill <- numeric(n)
   
-for (i in 1:length(actvals)){
-    mean = mean(actvals)
-    sd = sd(actvals)
-    corrskill[i] = cor(actvals[i],predvals[i])
-    rmseskill[i] = sqrt(mean((actvals[i] - predvals[i])^2))
+for (i in 1:n){
+  corrskill[i] <- cor(actvals[i], predvals[i])
+  rmseskill[i] <- sqrt(mean((actvals[i] - predvals[i])^2))
 }
 
+# Create data frames for plotting
+corr_df <- data.frame(metric = "Correlation", value = corrskill)
+rmse_df <- data.frame(metric = "RMSE", value = rmseskill)
+
+  
 # Boxplot the Correlation and RMSE
-corrplot <- ggplot()+
-  geom_boxplot(mapping = aes(x = "Correlation", corrskill))+
+corrplot <- ggplot(corr_df, aes(x = metric, y = value)) +
+  geom_boxplot() +
   labs(title = "Corr.", x = "", y = "")+
   theme(plot.title = element_text(size = 13, face = "bold", hjust = 0.5))+
   coord_cartesian(ylim = c(-0.5,2))
 
-rmseplot <- ggplot()+
-  geom_boxplot(mapping = aes(x = "RMSE", rmseskill))+
+rmseplot <- ggplot(rmse_df, aes(x = metric, y = value)) +
+  geom_boxplot() +
   labs(title = "RMSE", x = "", y = "")+
   theme(plot.title = element_text(size = 13, face = "bold", hjust = 0.5))+
   coord_cartesian(ylim = c(0,5000))
